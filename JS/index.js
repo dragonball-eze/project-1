225a4e035972809378dc1816b408fcb74c89cfc8
Special K <kelian.somon@gmail.com>
2021-08-23 19:01:27 +0200

    bullet impact on aliens only

--- JS/index.js
+++ JS/index.js
@@ -10,12 +10,20 @@ document.getElementById("space-board").style.display="none";//to make the game i
     startGame();
   };
 
-  let currentGame;
+let currentGame;
 
   //Sounds
   const shootSound = new Audio ()
   shootSound.src = "../sounds_Shoot.mp3";
 
+   //Bullet impact
+   function bulletHit(alien, bullet) {
+
+    return !(bullet.x > alien.x + alien.width ||
+    bullet.x < alien.x ||
+    bullet.y > alien.y + alien.height ||
+    bullet.y + bullet.height < alien.y)
+  }
 
   function startGame() {
     currentGame = new Game();
@@ -25,8 +33,10 @@ document.getElementById("space-board").style.display="none";//to make the game i
     bullet.drawBullet();
     cancelAnimationFrame(currentGame.animationId);
     updateCanvas();
+    
   }
 
+  // Satellites
   function drawSatellite() {
     currentGame.satellitesFrequency++;
     if (currentGame.satellitesFrequency % 200 === 1) {
@@ -46,10 +56,30 @@ document.getElementById("space-board").style.display="none";//to make the game i
     currentGame.satellites.forEach((satellite, index) => {
       satellite.y += 1;
       satellite.draw();
+
+      //Satellite collusion
+      function detectCollision(satellite) {
+        return !(
+          currentGame.player.moveLeft() > satellite.right() ||
+          currentGame.player.moveRight() < satellite.left() ||
+          currentGame.player.moveTop() > satellite.bottom()
+        );
+      }
+
+      if (detectCollision(satellite)) {
+        currentGame.gameOver = true;
+        currentGame.satellitesFrequency = 0;
+        currentGame.score = 0;
+        currentGame.satellites = [];
+        //document.getElementById("score").innerText = 0;
+        document.getElementById("space-board").style.display = "none";
+        cancelAnimationFrame(currentGame.animationId);
+        alert("Ouch! You crashed on a satellite!");
+      }
     });
 
   }
-
+  // Aliens
   function drawAlien() {
     currentGame.aliensFrequency++;
     if (currentGame.aliensFrequency % 100 === 1) {
@@ -67,16 +97,35 @@ document.getElementById("space-board").style.display="none";//to make the game i
     currentGame.aliens.forEach((alien, index) => {
       alien.y += 1;
       alien.draw();
+      
+      //Alien collision
+      function detectCollision(alien) { // TO CHECK
+        return !(
+          currentGame.player.moveLeft() > alien.right() ||
+          currentGame.player.moveRight() < alien.left() ||
+          currentGame.player.moveTop() > alien.bottom()
+        );
+      }
+
+      if (detectCollision(alien)) {
+        currentGame.gameOver = true;
+        currentGame.aliensFrequency = 0;
+        //currentGame.score = 0;
+        currentGame.aliens = [];
+        //document.getElementById("score").innerText = 0;
+        document.getElementById("space-board").style.display = "none";
+        cancelAnimationFrame(currentGame.animationId);
+        alert("Damn! They got you first!");
+      }
     });
 
   }
 
-  
-
+ 
   function updateCanvas() {
     context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight)
     currentGame.player.drawPlayer();
-    //drawObstacles();
+    //drawBullets();
     currentGame.bullets.forEach(bullet => {
       bullet.y -= 3;
       bullet.drawBullet();
@@ -84,8 +133,29 @@ document.getElementById("space-board").style.display="none";//to make the game i
 
     drawSatellite();
     drawAlien();
+
+
+
+    for(let j = 0; j < currentGame.aliens.length; j++){
+      for (let k = 0; k < currentGame.bullets.length; k++) {
+          if (bulletHit(currentGame.aliens[j],currentGame.bullets[k])){
+            console.log('collision');
+              currentGame.aliens.splice(j,1);
+              currentGame.bullets.splice(k,1);
+              //currentGame.score++;
+              //document.getElementById('score').innerHTML = currentGame.score;
+              //explosionSound.play();
+          }
+      }
+    }
+
+
+
     if (currentGame.gameOver === false) {
-      currentGame.animationId = requestAnimationFrame(updateCanvas);}
+      currentGame.animationId = requestAnimationFrame(updateCanvas);
+    }
+
+
   }
 
 
